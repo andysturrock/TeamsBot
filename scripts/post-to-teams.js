5617c9e38ef95042f8d68d0333593d4909716fa6
Andy Sturrock <github@sturrock.org>
2019-07-26 21:22:31 +0100

    Get to the point of being able to list teams.  Now need MS permission to access Protected APIs.

--- scripts/post-to-teams.js
+++ scripts/post-to-teams.js
@@ -36,12 +36,15 @@ function getBearerToken(robot) {
         }
         parsedBody = JSON.parse(body)
         access_token = parsedBody.access_token
+        if(!access_token) {
+          throw new Error("Cannot obtain token: " + body)
+        }
         resolve(access_token)
       });
   });
 }
 
-function getAllTeams(robot, access_token) {
+function getTeamData(robot, access_token) {
   return new Promise(function (resolve, reject) {
   robot.http(`https://graph.microsoft.com/beta/groups?$select=id,displayname,resourceProvisioningOptions`)
     .header('Authorization', `Bearer ${access_token}`)
@@ -67,10 +70,12 @@ function getAllGroupsWithTeams(robot, access_token) {
         throw(err)
       }
       if(!body) {
-        throw new Error("No teams found")
+        throw new Error("No groups found")
       }
-
       groups = JSON.parse(body).value
+      if(!groups) {
+        throw new Error("No groups found")
+      }
       groupsWithTeams = []
       groups.forEach((group) => {if(group.resourceProvisioningOptions == "Team") groupsWithTeams.push(group)})
       resolve(groupsWithTeams)
@@ -78,11 +83,79 @@ function getAllGroupsWithTeams(robot, access_token) {
   });
 }
 
+function getTeamData(robot, access_token, group_id) {
+  return new Promise(function (resolve, reject) {
+    robot.http(`https://graph.microsoft.com/beta/teams/${group_id}`)
+      .header('Authorization', `Bearer ${access_token}`)
+      .get()(function (err, resp, body) {
+        if(err) {
+          throw(err)
+        }
+        if(!body) {
+          throw new Error("Can't get data for group id " + group_id)
+        }
+        groups = JSON.parse(body)
+        resolve(groups)
+      });
+    });
+}
+
+function getChannelsInTeam(robot, access_token, team_id) {
+  
+  return new Promise(function (resolve, reject) {
+    robot.http(`https://graph.microsoft.com/beta/teams/${team_id}/channels`)
+      .header('Authorization', `Bearer ${access_token}`)
+      .get()(function (err, resp, body) {
+        if(err) {
+          throw(err)
+        }
+        if(!body) {
+          throw new Error("No channels found in team " + team_id)
+        }
+        channels = JSON.parse(body)
+        resolve(channels)
+      });
+    });
+}
+
+function getMessagesFromChannel(robot, access_token, team, channel) {
+  return new Promise(function (resolve, reject) {
+    robot.http(`https://graph.microsoft.com/beta/teams/{id}/channels/{id}/messages`)
+      .header('Authorization', `Bearer ${access_token}`)
+      .get()(function (err, resp, body) {
+        if(err) {
+          throw(err)
+        }
+        if(!body) {
+          throw new Error("No messages found in channel " + channel.name + " in team " + team.name)
+        }
+        console.log("messages raw: " + body)
+        console.log("messages: " + util.inspect(body))
+        console.log("resp.statusCode: " + resp.statusCode)
+        messages = JSON.parse(body)
+        resolve(messages)
+      });
+    });
+}
+
 async function doGraphStuff(robot) {
   access_token = await getBearerToken(robot)
 
   groupsWithTeams = await getAllGroupsWithTeams(robot, access_token)
-  console.log("groupsWithTeams: " + util.inspect(groupsWithTeams))
+
+  teams = []
+  for(const group of groupsWithTeams) {
+    teams_data = await getTeamData(robot, access_token, group.id)
+    channels = await getChannelsInTeam(robot, access_token, teams_data.id)
+    for(const channel of channels.value) {
+      console.log("Group: " + group.displayName + ", channel id = " + channel.id + " name = " + channel.displayName)
+    }
+  }
+
+  team = {id: "fb5cb1df-ad0a-4ae4-b979-21db4a48f68c", name: "fb5cb1df-ad0a-4ae4-b979-21db4a48f68c"}
+  channel = {id: "19:6e6280be5ce240b28eca8ee00de866c3@thread.skype", name: "slacktest channel 2"}
+
+  messages = await getMessagesFromChannel(robot, access_token, team, channel)
 }
 
 module.exports = function (robot) {
